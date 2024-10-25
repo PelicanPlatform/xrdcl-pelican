d176276160fcf73e61e7506e2acbbe0bbecf04a9
Brian Bockelman <bbockelman@morgridge.org>
2024-10-25 10:04:57 +0200

    Try hacking around a fixed Pelican bug

--- tests/pelican-setup.sh
+++ tests/pelican-setup.sh
@@ -49,6 +49,11 @@ cd "$RUNDIR"
 export XRD_PLUGINCONFDIR="$RUNDIR/client.plugins.d"
 mkdir -p "$XRD_PLUGINCONFDIR"
 
+# Hack for Pelican v7.10.x and GHA: $XRD_PLUGINCONFDIR is ignored until 7.11.0 and
+# only the system-level plugin directory can be used.  Try creating a symlink in a
+# well-known location so the GHA can symlink the system plugin.conf to it.
+ln -s "$XRD_PLUGINCONFDIR/pelican-plugin.conf" "$BINARY_DIR/tests/$TEST_NAME/pelican-plugin.conf"
+
 PLUGIN_SUFFIX=so
 if [ $(uname) = "Darwin" ]; then
   PLUGIN_SUFFIX=dylib
