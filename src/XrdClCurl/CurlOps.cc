26a67c8c4c70cdc6f6ad0fac5b7eafbdd8c81646
alja <amraktadel@ucsd.edu>
2025-06-04 13:16:25 -0700

    Remove extra ETag quotes in HeaderParser::Parse

--- src/XrdClCurl/CurlOps.cc
+++ src/XrdClCurl/CurlOps.cc
@@ -376,3 +376,20 @@ CurlOperation::WaitSocketCallback(std::string &err)
     }
     return m_conn_callout_result;
 }
+
+// ------------------------ BEGIN QUERY OPS -------
+//
+void CurlQueryOp::Success()
+{
+    SetDone(false); // ???
+    m_logger->Debug(kLogXrdClCurl, "CurlQueryOp::Success");
+
+    XrdCl::Buffer* qInfo = new XrdCl::Buffer();
+    qInfo->FromString(m_headers.GetETag());
+    auto obj = new XrdCl::AnyObject();
+    obj->Set(qInfo);
+
+
+    m_handler->HandleResponse(new XrdCl::XRootDStatus(), obj);
+    m_handler = nullptr;
+}
